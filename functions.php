a5237d94999e3006e20d41d83e68a06bdd40922f
Alan Rempel <alan.rempel@gmail.com>
2021-02-11 20:45:53 -0500

    Make CSS customizations work in the block editor too

--- functions.php
+++ functions.php
@@ -19,8 +19,13 @@ $greiffenberg_defaults = array(
   'body-line-height' => '1.6',
   'heading-line-height' => '1.4',
   'vertical-spacing' => '1',
-  'horizontal-spacing' => '0.8',
+  'horizontal-spacing' => '2',
   'site-title-case' => 'uppercase'
+);
+
+$greiffenberg_units = array(
+  'horizontal-spacing' => 'rem',
+  'vertical-spacing' => 'rem'
 );
 
 function greiffenberg_customize($customizer) {
@@ -114,21 +119,28 @@ function greiffenberg_customize($customizer) {
 add_action('customize_register', 'greiffenberg_customize');
 
 function greiffenberg_get_mod($id) {
-  global $greiffenberg_defaults;
-  return get_theme_mod($id, $greiffenberg_defaults[$id]);
+  global $greiffenberg_defaults, $greiffenberg_units;
+  return get_theme_mod($id, $greiffenberg_defaults[$id]) . ($greiffenberg_units[$id] ?? '');
 }
 
-function greiffenberg_custom_css() {
-  echo '<style> body {';
-  echo '--global--line-height-body:' . greiffenberg_get_mod('body-line-height') . ';'; 
-  echo '--global--line-height-heading:' . greiffenberg_get_mod('heading-line-height') . ';'; 
-  echo '--global--spacing-vertical:' . greiffenberg_get_mod('vertical-spacing') . 'rem;';
-  echo '--global--spacing-horizontal:' . greiffenberg_get_mod('horizontal-spacing') . 'rem;';
-  echo '--branding--title--text-transform:' . greiffenberg_get_mod('site-title-case') . ';';
-  echo '}</style>';
+function greiffenberg_css_variable($variable, $value, $important = false) {
+  return "--$variable: " . greiffenberg_get_mod($value) . ($important ? ' !important;' : ';');
 }
 
-add_action('wp_head', 'greiffenberg_custom_css');
+function greiffenberg_css_variables($important) {
+  return
+      greiffenberg_css_variable('global--line-height-body', 'body-line-height', $important)
+    . greiffenberg_css_variable('global--line-height-heading', 'heading-line-height', $important)
+    . greiffenberg_css_variable('global--spacing-vertical', 'vertical-spacing', $important)
+    . greiffenberg_css_variable('global--spacing-horizontal', 'horizontal-spacing', $important)
+    . greiffenberg_css_variable('branding--title--text-transform', 'site-title-case', $important);
+}
+
+function greiffenberg_inline_style() {
+  echo '<style>body {' . greiffenberg_css_variables(false) . '}</style>';
+}
+
+add_action('wp_head', 'greiffenberg_inline_style');
 
 function greiffenberg_customize_preview_script() {
   wp_enqueue_script(
@@ -140,3 +152,11 @@ function greiffenberg_customize_preview_script() {
 }
 
 add_action('customize_preview_init', 'greiffenberg_customize_preview_script');
+
+function greiffenberg_block_editor_inline_style() {
+  wp_add_inline_style('wp-edit-post', '.editor-styles-wrapper {' . greiffenberg_css_variables(true) . '}');
+}
+
+add_action('enqueue_block_editor_assets', 'greiffenberg_block_editor_inline_style');
+
+add_action('after_setup_theme', function () { add_editor_style('./block-editor-style.css'); });
