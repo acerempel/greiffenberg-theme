4e55bcf14d83964f347fcb44060548575c637152
Alan Rempel <alan.rempel@gmail.com>
2021-11-19 18:59:16 -0500

    Fix header background image

--- scripts/adjust-background-style.js
+++ scripts/adjust-background-style.js
@@ -1,8 +1,16 @@
-const styleElem = document.getElementById('custom-background-css');
-const style = styleElem.textContent.slice(5);
-styleElem.textContent = '.header-background' + style;
-document.addEventListener('DOMContentLoaded', () => {
-  if (document.body.classList.contains('custom-background')) {
-    document.querySelector('.header-background').classList.add('custom-background');
-  }
-})
+(function (){
+  const styleElem = document.getElementById('custom-background-css');
+  const styleText = styleElem.textContent;
+  // styleText = " body.custom-background { …"
+  const match = styleText.match(/^\s*\w+(\.[a-zA-Z-]+)/);
+  if (match.length < 1) return;
+  const style = styleText.slice(match[0].length);
+  const origClass = match[1];
+  // styleElem.textContent = ".header-background.custom-background::before { …"
+  styleElem.textContent = '.header-background' + origClass + '::before ' + style;
+  document.addEventListener('DOMContentLoaded', () => {
+    if (document.body.classList.contains('custom-background')) {
+      document.querySelector('.header-background').classList.add('custom-background');
+    }
+  })
+})();
